f39efd77954e06704f63fd5168bea475bb68da5b
lukemyh123 <46793748+lukemyh123@users.noreply.github.com>
2019-02-22 00:03:52 -0800

    Add files via upload

--- Actor.h
+++ Actor.h
@@ -9,118 +9,128 @@ class StudentWorld;
 class Actor : public GraphObject
 {
 public:
-    Actor(int imageID, double startX, double startY, Direction dir, int depth, StudentWorld *this_world);
-    virtual void doSomething() = 0;
-    bool getStatus() { return object_alive; }
-    virtual void setDead() { object_alive = false; }
-    virtual void setAlive() { object_alive = true; }
-    StudentWorld * getWorld() { return s_world; }
-    virtual bool isBlockActor() = 0;
-    virtual bool canBeDamagedByFlame() = 0;
-    virtual bool isAFlame() { return false;}
-    virtual bool isExit(){return false;}
-    virtual bool person() { return false; }
-    ~Actor();
+	Actor(int imageID, double startX, double startY, Direction dir, int depth, StudentWorld *this_world);
+	virtual void doSomething() = 0;
+	bool getStatus() { return object_alive; }
+	virtual void setDead() { object_alive = false; }
+	virtual void setAlive() { object_alive = true; }
+	StudentWorld * getWorld() { return s_world; }
+	virtual bool isBlockActor() = 0;
+	virtual bool canBeDamagedByFlame() = 0;
+	virtual bool isAFlame() { return false; }
+	virtual bool isExit() { return false; }
+	virtual bool person() { return false; }
+	~Actor();
 private:
-    bool object_alive = true;
-    StudentWorld* s_world;
+	bool object_alive = true;
+	StudentWorld* s_world;
 };
 
 class Penelope : public Actor
 {
 public:
-    Penelope(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return true; }
-    virtual bool canBeDamagedByFlame() { return true; }
-    virtual bool person() { return true; }
+	Penelope(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return true; }
+	virtual bool canBeDamagedByFlame() { return true; }
+	virtual bool person() { return true; }
 private:
 };
 
 class Wall : public Actor
 {
 public:
-    Wall(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething() {}
-    virtual bool isBlockActor() { return true; }
-    virtual bool canBeDamagedByFlame() { return false; }
+	Wall(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething() {}
+	virtual bool isBlockActor() { return true; }
+	virtual bool canBeDamagedByFlame() { return false; }
 private:
 };
 
 class Exit : public Actor  // 3 for Exit
 {
 public:
-    Exit(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return false; }
-    virtual bool isExit(){return true;}
+	Exit(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return false; }
+	virtual bool isExit() { return true; }
 };
 
 class Pit : public Actor
 {
 public:
-    Pit(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return false; }
-    
+	Pit(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return false; }
+
 };
 
 class Flame : public Actor
 {
 public:
-    Flame(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return false; }
-    virtual bool isAFlame() { return true; }
-    
-private:
-    int tick = 0;
-};
+	Flame(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return false; }
+	virtual bool isAFlame() { return true; }
 
+private:
+	int tick = 0;
+};
 
 class Vaccine_goodie : public Actor
 {
 public:
-    Vaccine_goodie(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return true; }
-    
+	Vaccine_goodie(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return true; }
+
 private:
 };
 
 class Gas_can_goodie : public Actor
 {
 public:
-    Gas_can_goodie(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return true; }
+	Gas_can_goodie(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return true; }
 };
 
 class Landmine_goodie : public Actor
 {
 public:
-    Landmine_goodie(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return true; }
+	Landmine_goodie(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return true; }
 };
 
 class Landmine : public Actor
 {
 public:
-    Landmine(double startX, double startY, StudentWorld *this_world);
-    virtual void doSomething();
-    virtual bool isBlockActor() { return false; }
-    virtual bool canBeDamagedByFlame() { return true; }
-    
+	Landmine(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return false; }
+	virtual bool canBeDamagedByFlame() { return true; }
+
 private:
-    int tick = 30;
+	int tick = 30;
 };
 
-
+class DumbZombie :public Actor
+{
+public:
+	DumbZombie(double startX, double startY, StudentWorld *this_world);
+	virtual void doSomething();
+	virtual bool isBlockActor() { return true; }
+	virtual bool canBeDamagedByFlame() { return true; }
+	virtual bool person() { return true; }
+private:
+	int tick;
+	int movement_plan;
+};
 #endif // ACTOR_H_
--- StudentWorld.h
+++ StudentWorld.h
@@ -12,54 +12,54 @@
 class StudentWorld : public GameWorld
 {
 public:
-    StudentWorld(std::string assetPath);
-    ~StudentWorld();
-    virtual int init();
-    virtual int move();
-    virtual void cleanUp();
-    
-    bool check_collision(double next_x, double next_y);
-    void Player_overlapWithExit(double exit_x, double exit_y);
-    bool ExitBlock_Flame(double x, double y);  //Exit needs to block flame
-    bool moveToNextLevel() { return go_next_level; }
-    bool citizen_overlapWithExit(double exit_x, double exit_y);
-    
-    bool Player_overlapWith_VaccineGoodies(double goodies_x, double goodies_y);
-    bool Player_overlapWith_GasCanGoodies(double goodies_x, double goodies_y);
-    bool Player_overlapWith_LandmineGoodies(double goodies_x, double goodies_y);
-    
-    void overlapWithPit(double pit_x, double pit_y);
-    void overlapWithFlame(double flame_x, double flame_y);
-    
-    void fire(double x, double y, int dir);
-    
-    void placeLandmine(double x, double y);
-    void landmineBoom(double landmine_x, double landmine_y);
-    bool overlapwithLandmine(double x, double y);
-    
-    void reset_goodies() { vaccine = 0; flamethrower = 0; landmines = 0; }
-    void add_vaccine() { vaccine++; }
-    void reduce_vaccine() { vaccine--; }
-    void add_flamethrower() { flamethrower += 5; }
-    void reduce_flamethrower() { flamethrower--; }
-    void add_landmines() { landmines += 2; }
-    void reduce_landmines() { landmines--; }
-    int get_vaccine() { return vaccine; }
-    int get_flamethrower() { return flamethrower; }
-    int get_landmines() { return landmines; }
-    
-    void setGame_info();
-    
-    
-    std::string check_actorsPos(int x, int y);
+	StudentWorld(std::string assetPath);
+	~StudentWorld();
+	virtual int init();
+	virtual int move();
+	virtual void cleanUp();
+
+	bool check_collision(double next_x, double next_y);
+	void Player_overlapWithExit(double exit_x, double exit_y);
+	bool ExitBlock_Flame(double x, double y);  //Exit needs to block flame
+	bool moveToNextLevel() { return go_next_level; }
+	bool citizen_overlapWithExit(double exit_x, double exit_y);
+
+	bool Player_overlapWith_VaccineGoodies(double goodies_x, double goodies_y);
+	bool Player_overlapWith_GasCanGoodies(double goodies_x, double goodies_y);
+	bool Player_overlapWith_LandmineGoodies(double goodies_x, double goodies_y);
+
+	void overlapWithPit(double pit_x, double pit_y);
+	void overlapWithFlame(double flame_x, double flame_y);
+
+	void fire(double x, double y, int dir);
+
+	void placeLandmine(double x, double y);
+	void landmineBoom(double landmine_x, double landmine_y);
+	bool overlapwithLandmine(double x, double y);
+
+	void reset_goodies() { vaccine = 0; flamethrower = 0; landmines = 0; }
+	void add_vaccine() { vaccine++; }
+	void reduce_vaccine() { vaccine--; }
+	void add_flamethrower() { flamethrower += 5; }
+	void reduce_flamethrower() { flamethrower--; }
+	void add_landmines() { landmines += 2; }
+	void reduce_landmines() { landmines--; }
+	int get_vaccine() { return vaccine; }
+	int get_flamethrower() { return flamethrower; }
+	int get_landmines() { return landmines; }
+
+	void setGame_info();
+
+
+	std::string check_actorsPos(int x, int y);
 private:
-    std::vector<Actor*> m_actors;
-    Penelope* m_penelope;
-    bool go_next_level = false;
-    bool ExitBlockFlame = false;
-    int vaccine = 0;
-    int flamethrower = 0;
-    int landmines = 0;
+	std::vector<Actor*> m_actors;
+	Penelope* m_penelope;
+	bool go_next_level = false;
+	bool ExitBlockFlame = false;
+	int vaccine = 0;
+	int flamethrower = 0;
+	int landmines = 0;
 };
 
 #endif // STUDENTWORLD_H_
